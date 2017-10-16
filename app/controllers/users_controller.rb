a207ca6c4d26baf9d9db32a9a43397c88cae5c7a
kasumi <kasumi0201@gmail.com>
2017-10-17 10:43:02 +1100

    all good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
    before_action :admin_user,     only: :destroy
+
   def following
     @user  = User.find(params[:id])
     @users = @user.following
@@ -16,10 +17,7 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-    # @user.name = User.find_by_id(params[:id])
-    # @user.id = User.find_by_id(params[:id])
     @photos = Photo.where(user_id: @user.id)
-    # @photos = @user.photos.paginate(page: params[:page])
   end
 
   def create
@@ -36,12 +34,6 @@ class UsersController < ApplicationController
       @users = User.find(params[:id])
       # User.where(activated: FILL_IN).paginate(page: params[:page])
     end
-
-    # def show
-    #   @user = User.find(params[:id])
-    #   redirect_to root_url
-    #   # and return unless FILL_IN
-    # end
 
   def follow
   end
